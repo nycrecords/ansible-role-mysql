3b253453a909e52ba8ac07d2a43acee71d5eed5a
Gary Zhou <gzhou@records.nyc.gov>
2020-06-03 10:24:51 -0400

    Update tasks for SCL

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -9,27 +9,14 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 @pytest.mark.parametrize("name", [
-    "rh-php72",
-    "rh-php72-php-devel",
-    "rh-php72-php-fpm",
-    "rh-php72-php-gd",
-    "rh-php72-php-ldap",
-    "rh-php72-php-mbstring",
-    "rh-php72-php-opcache",
-    "rh-php72-php-pdo",
-    "rh-php72-php-pecl-apcu",
-    "rh-php72-php-xmlrpc",
+    "rh-mariadb103",
+    "MySQL-python",
+    "perl-DBD-MySQL",
 ])
-def test_php_packages_installed(host, name):
+def test_mysql_packages_installed(host, name):
     pkg = host.package(name)
     assert pkg.is_installed
 
-def test_fpm_service(host):
-    service = host.service("rh-php72-php-fpm")
-    assert service.is_running
-    assert service.is_enabled
-
-def test_php_info_version(host):
-    cmd = 'curl localhost/test.php'
-    out = host.check_output(cmd)
-    assert re.search('PHP Version 7.2.24', out)
+def test_mysql_listening(host):
+    socket = host.socket("tcp://127.0.0.1:3306")
+    assert socket.is_listening
