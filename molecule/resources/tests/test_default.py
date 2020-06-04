81d4124e54cfbf714e3eb40e24a6bf783c582801
Gary Zhou <gzhou@records.nyc.gov>
2020-06-04 15:23:00 -0400

    Update default converge and add tests

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -20,3 +20,13 @@ def test_mysql_packages_installed(host, name):
 def test_mysql_listening(host):
     socket = host.socket("tcp://127.0.0.1:3306")
     assert socket.is_listening
+
+def test_mysql_user(host):
+    cmd = 'mysql -u root -proot -e "select user from mysql.user;"'
+    out = host.check_output(cmd)
+    assert "jdoe" in out
+
+def test_mysql_database(host):
+    cmd = 'mysql -u root -proot -e "show databases;"'
+    out = host.check_output(cmd)
+    assert "example" in out
